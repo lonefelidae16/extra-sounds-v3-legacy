c5a702a918dc4783918e7d4119903b4421a8f725
lonefelidae16 <kow161206@gmail.com>
2026-06-06 16:05:00 +0900

    Fix crash on startup with REI: 1.16.5

--- versioned/es1.16.5/src/main/java/dev/stashy/extrasounds/mc1_16_5/compat/mixin/rei/TextFieldWidgetMixin.java
+++ versioned/es1.16.5/src/main/java/dev/stashy/extrasounds/mc1_16_5/compat/mixin/rei/TextFieldWidgetMixin.java
@@ -1,8 +1,7 @@
 package dev.stashy.extrasounds.mc1_16_5.compat.mixin.rei;
 
 import dev.stashy.extrasounds.logics.impl.TextFieldHandler;
-import me.shedaniel.rei.api.client.gui.widgets.TextField;
-import me.shedaniel.rei.impl.client.gui.widget.basewidgets.TextFieldWidget;
+import me.shedaniel.rei.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -15,30 +14,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
 @Mixin(TextFieldWidget.class)
-public abstract class TextFieldWidgetMixin implements TextField {
+public abstract class TextFieldWidgetMixin {
     @Unique
     private final TextFieldHandler soundHandler = new TextFieldHandler();
 
     @Shadow(remap = false)
-    protected int cursorPos;
+    protected int selectionStart;
+    @Shadow(remap = false)
+    protected int selectionEnd;
+    @Shadow(remap = false)
+    public abstract String getText();
+    @Shadow(remap = false)
+    public abstract String getSelectedText();
 
     @Inject(method = "erase", at = @At("HEAD"), remap = false)
     private void extrasounds$eraseStrHead(int offset, CallbackInfo ci) {
-        this.soundHandler.onCharErase(offset, this.getText().length(), this.cursorPos, this.cursorPos);
+        this.soundHandler.onCharErase(offset, this.getText().length(), this.selectionStart, this.selectionEnd);
     }
 
     @Inject(method = "erase", at = @At("RETURN"), remap = false)
     private void extrasounds$eraseStrReturn(CallbackInfo ci) {
-        this.soundHandler.setCursor(this.cursorPos);
+        this.soundHandler.setCursor(this.selectionEnd);
     }
 
     @Inject(method = "charTyped", at = @At("RETURN"))
     private void extrasounds$appendChar(CallbackInfoReturnable<Boolean> cir) {
-        if (!cir.getReturnValue() || !this.soundHandler.isPosUpdated(this.cursorPos, this.cursorPos)) {
+        if (!cir.getReturnValue() || !this.soundHandler.isPosUpdated(this.selectionStart, this.selectionEnd)) {
             return;
         }
         this.soundHandler.onKey(TextFieldHandler.KeyType.INSERT);
-        this.soundHandler.setCursor(this.cursorPos);
+        this.soundHandler.setCursor(this.selectionEnd);
     }
 
     @Inject(
@@ -54,7 +59,7 @@ public abstract class TextFieldWidgetMixin implements TextField {
             return;
         }
         this.soundHandler.onKey(TextFieldHandler.KeyType.CUT);
-        this.soundHandler.setCursor(this.cursorPos);
+        this.soundHandler.setCursor(this.selectionEnd);
     }
 
     @Inject(
@@ -66,27 +71,27 @@ public abstract class TextFieldWidgetMixin implements TextField {
             )
     )
     private void extrasounds$pasteAction(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (!Screen.isPaste(keyCode) || !this.soundHandler.isPosUpdated(this.cursorPos, this.cursorPos)) {
+        if (!Screen.isPaste(keyCode) || !this.soundHandler.isPosUpdated(this.selectionStart, this.selectionEnd)) {
             return;
         }
         this.soundHandler.onKey(TextFieldHandler.KeyType.PASTE);
-        this.soundHandler.setCursor(this.cursorPos);
+        this.soundHandler.setCursor(this.selectionEnd);
     }
 
     @Inject(method = "keyPressed",
             at = {
-                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/impl/client/gui/widget/basewidgets/TextFieldWidget;moveCursor(I)V", shift = At.Shift.AFTER),
-                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/impl/client/gui/widget/basewidgets/TextFieldWidget;moveCursorTo(I)V", shift = At.Shift.AFTER),
-                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/impl/client/gui/widget/basewidgets/TextFieldWidget;moveCursorToStart()V", shift = At.Shift.AFTER),
-                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/impl/client/gui/widget/basewidgets/TextFieldWidget;moveCursorToEnd()V", shift = At.Shift.AFTER)
+                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/gui/widget/TextFieldWidget;moveCursor(I)V", shift = At.Shift.AFTER),
+                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/gui/widget/TextFieldWidget;setCursor(I)V", shift = At.Shift.AFTER),
+                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/gui/widget/TextFieldWidget;setCursorToStart()V", shift = At.Shift.AFTER),
+                    @At(value = "INVOKE", target = "Lme/shedaniel/rei/gui/widget/TextFieldWidget;setCursorToEnd()V", shift = At.Shift.AFTER)
             }
     )
     private void extrasounds$cursorMoveKeyTyped(CallbackInfoReturnable<Boolean> cir) {
-        this.soundHandler.onCursorChanged(this.cursorPos, this.cursorPos);
+        this.soundHandler.onCursorChanged(this.selectionStart, this.selectionEnd);
     }
 
-    @Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lme/shedaniel/rei/impl/client/gui/widget/basewidgets/TextFieldWidget;moveCursorTo(I)V", shift = At.Shift.AFTER))
+    @Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lme/shedaniel/rei/gui/widget/TextFieldWidget;setCursor(I)V", shift = At.Shift.AFTER))
     private void extrasounds$clickEvent(CallbackInfoReturnable<Boolean> cir) {
-        this.soundHandler.onCursorChanged(this.cursorPos, this.cursorPos);
+        this.soundHandler.onCursorChanged(this.selectionStart, this.selectionEnd);
     }
 }
